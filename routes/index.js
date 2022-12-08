f8813973633f6685d120fbd32002a03679749b64
unknown <benmen1980@gmail.com>
2022-12-08 23:06:41 +0200

    add versiont

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Priority Node service by SImplyCT' });
+  res.render('index', { title: 'Priority Node service by SImplyCT',
+                                      version: process.env.npm_package_version });
 });
 
 module.exports = router;
